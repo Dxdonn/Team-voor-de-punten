ee400e8745c170bff88bfba103ae04fbe5c3f829
LexBlom <lexblom2002@gmail.com>
2024-11-28 14:39:28 +0100

    Contactpagina deel2

--- bedankt.php
+++ bedankt.php
@@ -1,13 +1,7 @@
 <html>
 <body>
-
-Bedankt voor het invullen  <?php echo $_POST["name"]; ?>
-We zullen u binnenkort via <?php echo $_POST["email"]; ?> contacteren .
-
-Met vriendelijke groet,
-Team voor de punten
-
 <?php
+wordwrap("Bedankt voor het invullen ".$_POST['name']."\nWe zullen u binnenkort via".$_POST['email']."contacteren.\n\n Met vriendelijke groet,\nTeam voor de punten");
 
 // use wordwrap() because lines are longer than 70 characters
 $msg = wordwrap("Bedankt voor het invullen ".$_POST['name']."\nWe zullen u binnenkort via".$_POST['email']."contacteren.\n\n Met vriendelijke groet,\nTeam voor de punten");
