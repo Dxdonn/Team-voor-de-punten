fd8d3addbdf373b8b9064f5405f3179a8f65eaec
LexBlom <lexblom2002@gmail.com>
2024-11-28 14:30:21 +0100

    Mail versturen

--- bedankt.php
+++ bedankt.php
@@ -1,8 +1,19 @@
 <html>
 <body>
 
-Bedankt voor het invullen  <?php echo $_POST["name"]; ?><br>
+Bedankt voor het invullen  <?php echo $_POST["name"]; ?>
+We zullen u binnenkort via <?php echo $_POST["email"]; ?> contacteren .
 
+Met vriendelijke groet,
+Team voor de punten
 
+<?php
+
+// use wordwrap() because lines are longer than 70 characters
+$msg = "Bedankt voor het invullen ".echo $_POST['name']."\nWe zullen u binnenkort via $_POST['email']contacteren.\n\n Met vriendelijke groet,\nTeam voor de punten";
+
+// send email
+mail($_POST["email"],"Forms",$msg);
+?>
 </body>
 </html>
