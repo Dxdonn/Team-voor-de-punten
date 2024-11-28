44f4094fdde515673b658d1828021aa2b549483c
LexBlom <lexblom2002@gmail.com>
2024-11-28 14:34:28 +0100

    mail

--- bedankt.php
+++ bedankt.php
@@ -10,7 +10,7 @@ Team voor de punten
 <?php
 
 // use wordwrap() because lines are longer than 70 characters
-$msg = "Bedankt voor het invullen ".echo $_POST['name']."\nWe zullen u binnenkort via $_POST['email']contacteren.\n\n Met vriendelijke groet,\nTeam voor de punten";
+$msg = wordwrap("Bedankt voor het invullen ".$_POST['name']."\nWe zullen u binnenkort via".$_POST['email']."contacteren.\n\n Met vriendelijke groet,\nTeam voor de punten");
 
 // send email
 mail($_POST["email"],"Forms",$msg);
