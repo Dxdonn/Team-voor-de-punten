a38b393ef14f1caa32bbf81c7da5551614ebf259
LexBlom <lexblom2002@gmail.com>
2024-11-28 15:02:20 +0100

    Bedanktpaginafix

--- bedankt.php
+++ bedankt.php
@@ -1,10 +1,14 @@
 <html>
 <body>
 <?php
-wordwrap("Bedankt voor het invullen ".$_POST['name']."\nWe zullen u binnenkort via".$_POST['email']."contacteren.\n\n Met vriendelijke groet,\nTeam voor de punten");
+
+echo "Bedankt voor het invullen " . $_POST['name'] . "!<br>";
+echo "We zullen u binnenkort via " . $_POST['email'] . " contacteren. <br><br>";
+echo "Met vriendelijke groet,";
+echo "<br><br>Team voor de punten";
 
 // use wordwrap() because lines are longer than 70 characters
-$msg = wordwrap("Bedankt voor het invullen ".$_POST['name']."\nWe zullen u binnenkort via".$_POST['email']."contacteren.\n\n Met vriendelijke groet,\nTeam voor de punten");
+$msg = wordwrap("Bedankt voor het invullen ". $_POST['name']."\nWe zullen u binnenkort via". $_POST['email']."contacteren.\n\n Met vriendelijke groet,\nTeam voor de punten");
 
 // send email
 mail($_POST["email"],"Forms",$msg);
