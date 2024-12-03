1a119b3ea42ecc5fed8b28841ed28d80c9e2b260
Dexterdy <j.p.hagedoorn@proton.me>
2024-12-03 13:17:58 +0100

    add a page that uses breakpoints

---
+++ responsive/responsive.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+  <title>Bootstrap Demo Page</title>
+  <?php
+  $prefix = '../';
+  require '../head.php';
+  ?>
+  <link rel="stylesheet" href="responsive.css">
+</head>
+
+<body>
+  <?php
+  $prefix = '../';
+  require '../navigation.php';
+  ?>
+  <main id="content">
+  </main>
+</body>
+
+</html>
