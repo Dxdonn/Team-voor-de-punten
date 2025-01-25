231a9d2f072621beb8bc687bebd9b035b5244541
Dexterdy <j.p.hagedoorn@proton.me>
2025-01-25 14:06:34 +0100

    run a formatter on the very ugly html code

--- src/ionicons.js
+++ src/ionicons.js
@@ -1,27 +1,29 @@
-
-(function(doc){
+(function (doc) {
   var scriptElm = doc.scripts[doc.scripts.length - 1];
-  var warn = ['[ionicons] Deprecated script, please remove: ' + scriptElm.outerHTML];
+  var warn = [
+    "[ionicons] Deprecated script, please remove: " + scriptElm.outerHTML,
+  ];
 
-  warn.push('To improve performance it is recommended to set the differential scripts in the head as follows:')
+  warn.push(
+    "To improve performance it is recommended to set the differential scripts in the head as follows:"
+  );
 
-  var parts = scriptElm.src.split('/');
+  var parts = scriptElm.src.split("/");
   parts.pop();
-  parts.push('ionicons');
-  var url = parts.join('/');
+  parts.push("ionicons");
+  var url = parts.join("/");
 
-  var scriptElm = doc.createElement('script');
-  scriptElm.setAttribute('type', 'module');
-  scriptElm.src = url + '/ionicons.esm.js';
+  var scriptElm = doc.createElement("script");
+  scriptElm.setAttribute("type", "module");
+  scriptElm.src = url + "/ionicons.esm.js";
   doc.head.appendChild(scriptElm);
   warn.push(scriptElm.outerHTML);
 
-  scriptElm = doc.createElement('script');
-  scriptElm.setAttribute('nomodule', '');
-  scriptElm.src = url + '/ionicons.js';
+  scriptElm = doc.createElement("script");
+  scriptElm.setAttribute("nomodule", "");
+  scriptElm.src = url + "/ionicons.js";
   doc.head.appendChild(scriptElm);
   warn.push(scriptElm.outerHTML);
 
-  console.warn(warn.join('\n'));
-
+  console.warn(warn.join("\n"));
 })(document);
